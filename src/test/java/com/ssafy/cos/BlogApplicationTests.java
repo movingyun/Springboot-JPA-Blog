aa8e8742e6bfb842b9dc29e9e92753086e31762a
Dongyun You <ydy1107@naver.com>
2022-04-27 22:44:43 +0900

    환경세팅 완료

---
+++ src/test/java/com/ssafy/cos/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
