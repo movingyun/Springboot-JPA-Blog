3a8ff34d494a40b3c7402fbe729ef026c439d94d
유동윤 <ydy1107@naver.com>
2022-06-15 22:01:21 +0900

    ch22_연관관계의 주인

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -38,9 +41,14 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; //조회수
 	
-	@ManyToOne //객체관계 생성 //Many = Board, User = One -> 한명의 유저는 여러개의 게시물 작성 
+	@ManyToOne(fetch = FetchType.EAGER) //객체관계 생성 //Many = Board, User = One -> 한명의 유저는 여러개의 게시물 작성 
 	@JoinColumn(name="userId") //userId라는 컬럼으로 만들어
 	private User user; //DB는 오브젝트를 저장할수없어서 FK사용. But 자바는 오브젝트를 저장할 수 있다.
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) //하나의 게시글엔 여러개의 답변이 달린다.
+	//mappedBy 연과관계의 주인이 아니다(난 FK가 아니다) DB에 컬럼을 추가하지 않는다.
+	//내가 FK가 아니라 Reply의 board가 FK야!
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
