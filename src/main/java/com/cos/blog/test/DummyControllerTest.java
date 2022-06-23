341b72625d632b19f9894381359a06e113f25a67
유동윤 <ydy1107@naver.com>
2022-06-23 16:41:03 +0900

    ch28_update테스트

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -23,11 +27,32 @@ public class DummyControllerTest {
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepositoy;
 	
+	//업데이트
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id,@RequestBody User requestUser) {
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getPassword());
+		
+		User originUser = userRepositoy.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		originUser.setPassword(requestUser.getPassword());
+		originUser.setEmail(requestUser.getEmail());
+//		userRepositoy.save(originUser);
+		
+		return null;
+	}
+	
+	
+	//전체 유저 가져오기
 	@GetMapping("/dummy/users")
 	public List<User> list(){
 		return userRepositoy.findAll();
 	}
 	
+	//유저 가져와서 페이징하기
 	//한페이지당 2건의 데이터를 리턴
 	@GetMapping("/dummy/user")
 	public List<User> pageList(@PageableDefault(size = 2,sort = "id",direction = org.springframework.data.domain.Sort.Direction.DESC) Pageable pageable){
