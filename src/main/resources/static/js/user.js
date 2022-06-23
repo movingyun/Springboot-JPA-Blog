9e5d91a178b39173f7af7a2cb2e945679dfbae2f
유동윤 <ydy1107@naver.com>
2022-06-24 01:19:37 +0900

    ch35_회원가입을 위한 기초세팅

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,18 @@
+let index={
+	init: function(){
+		$("#btn-save").on("click",()=>{
+			this.save();
+		});
+	},
+
+	save:function(){
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		}
+		console.log(data);
+	}
+}
+
+index.init();
