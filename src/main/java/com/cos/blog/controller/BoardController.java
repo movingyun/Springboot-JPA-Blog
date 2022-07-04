e9020df15d491c6338e7b3bc24e58349f0b336d0
유동윤 <ydy1107@naver.com>
2022-07-05 01:36:52 +0900

    ch55_글 목록 페이징하기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.cos.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,8 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@GetMapping({"","/"})//아무것도 안적을 때, /일때 일로온다.
-	public String index(Model model) { 
-		model.addAttribute("boards",boardService.글목록());
+	public String index(Model model, @PageableDefault(size = 3,sort = "id",direction = org.springframework.data.domain.Sort.Direction.DESC) Pageable pageable) { 
+		model.addAttribute("boards",boardService.글목록(pageable));
 		return "index";
 	}
 	
