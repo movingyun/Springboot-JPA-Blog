52829e0962cbbd5a00884aa309a8db273b77a5c5
유동윤 <ydy1107@naver.com>
2022-07-05 01:16:59 +0900

    ch54_글 목록보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,16 +1,21 @@
 package com.cos.blog.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping({"","/"})//아무것도 안적을 때, /일때 일로온다.
-	public String index() { //컨트롤러에서 세션을 어떻게 찾지?
+	public String index(Model model) { 
+		model.addAttribute("boards",boardService.글목록());
 		return "index";
 	}
 	
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.cos.blog.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +22,10 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		BoardRepository.save(board);
+	}
+
+
+	public List<Board> 글목록() {
+		return BoardRepository.findAll();
 	}
 }
