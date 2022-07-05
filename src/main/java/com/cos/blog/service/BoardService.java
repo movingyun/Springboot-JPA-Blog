1124bea4d55fc8e12cb659317aa599a6dece3ccb
유동윤 <ydy1107@naver.com>
2022-07-06 00:04:25 +0900

    ch58_글 삭제하기

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -24,16 +24,21 @@ public class BoardService {
 		boardRepository.save(board);
 	}
 
-	
+	@Transactional(readOnly = true) //select만 하는곳엔 readOnly속성 붙혀줌
 	public Page<Board> 글목록(Pageable pageable) {
 		return boardRepository.findAll(pageable);
 	}
 
-
+	@Transactional(readOnly = true)
 	public Board 글상세보기(int id) {
 		return boardRepository.findById(id)
 				.orElseThrow(()->{
 					return new IllegalArgumentException("글 상세보기 실패");
 				});
+	}
+
+	@Transactional
+	public void 글삭제하기(int id) {
+		boardRepository.deleteById(id);
 	}
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,9 @@ let index={
 		$("#btn-save").on("click",()=>{
 			this.save();
 		});
+		$("#btn-delete").on("click",()=>{
+			this.deleteById();
+		});
 	},	
 
 	save:function(){
@@ -20,6 +23,23 @@ let index={
 		}).done(function(resp){
 			// 요청이 성공하면 ㄱㄱ
 			alert("글쓰기가 완료되었습니다.")
+			location.href="/";
+		}).fail(function(error){
+			// 요청이 실패하면 ㄱㄱ
+			alert(JSON.stringify(error));
+		});
+	},
+	deleteById:function(){
+		var id = $("#id").text();
+		console.log(id);
+		$.ajax({
+			// 회원가입 수행 요청
+			type : "DELETE",
+			url:"/api/board/"+id,
+			dataType:"json" 
+		}).done(function(resp){
+			// 요청이 성공하면 ㄱㄱ
+			alert("삭제가 완료되었습니다.")
 			location.href="/";
 		}).fail(function(error){
 			// 요청이 실패하면 ㄱㄱ
